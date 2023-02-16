dc1b41a69f5c566638e69c11e16f3f3d19d1827e
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-02-16 20:15:48 +0545

    herobanner

--- gym/src/components/HeroBanner.js
+++ gym/src/components/HeroBanner.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import {Box, Stack, Typography, Button} from '@mui/material';
+import HeroBannerImage from "../assets/images/banner.png";
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <>
+    <Box sx={
+      {mt: {lg: '212px', xs:'70px'},
+    ml:{sm: '50px'}}} posiotion="relative" p="20px">
+      <Typography color="#ff2625" fontWeight={"600"} fontSize='26px'>
+        Fitness Club
+      </Typography>
+      <Typography fontWeight={700}
+      sx={{fontSize:{lg:'44px' , xs:'40px'}}} mb="23px" mt="30px">
+        Sweat, Smile <br/> Repeat
+      </Typography>
+      <Typography fontSize="22px" lineHeight={"35px"} mb="4px"  >
+        Check out the most effective exercises.
+      </Typography>
+      <Button sx={{backgroundColor:'#ff22625', padding:'10px'}}variant="contained" color="error" href="exercises">Explore Exercises
+      </Button>
+      <Typography fontWeight="600px" color="#ff2625"
+      sx={{
+        opacity:0.1,
+        display:{lg: 'block' , xs:'none'}
+      }} fontSize='200px'> 
+        Exercise
+      </Typography>
+      <img src={HeroBannerImage} className="hero-banner-img" alt="banner"/>
+    </Box>
+    </>
   )
 }
 
