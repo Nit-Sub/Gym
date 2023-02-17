1ef3cb0b12a90e286900bbd1a3e2da3115bd70c4
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-02-17 18:22:17 +0545

    Update SearchExercises.js

--- gym/src/components/SearchExercises.js
+++ gym/src/components/SearchExercises.js
@@ -4,20 +4,22 @@ import { Box, TextField, Typography, Stack, Button } from '@mui/material'
 const SearchExercises = () => {
   return (
     <>
-      <Stack justifyContent={'center'} mt="37px" alignItem="center" p="20px">
+      <Stack justifyContent='center' mt="37px" alignItem="center" p="20px">
         <Typography fontWeight={700} sx={{
           fontSize:{lg:'44px' , xs:'30px'}
-        }} mb="50px" textAlign={"center"}>
+        }} mb="50px" textAlign="center">
           AweSome Exercise You <br/> Should Know.
-                </Typography>
+                </Typography>  
                 <Box position="relative" mb="72px">
-                  <TextField
-                  height="76px"
-                  placeholder='Search Exercise'
-                  value=""
-                  type="text"
-                  onChange={(e)=>{}}
-                  />
+                <TextField
+          height="76px"
+          sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
+          
+          onChange={(e) => {}}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button className="search-btn" variant="contained" color="error">Search</Button>
 
                 </Box>
       </Stack>
