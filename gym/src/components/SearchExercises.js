a0a6956452e5aebd72d0b54a5e7d2c8e90d78bf0
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-02-17 18:04:57 +0545

    Update SearchExercises.js

--- gym/src/components/SearchExercises.js
+++ gym/src/components/SearchExercises.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Box, TextField, Typography, Stack, Button } from '@mui/material'
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <>
+      <Stack justifyContent={'center'} mt="37px" alignItem="center" p="20px">
+        <Typography fontWeight={700} sx={{
+          fontSize:{lg:'44px' , xs:'30px'}
+        }} mb="50px" textAlign={"center"}>
+          AweSome Exercise You <br/> Should Know.
+                </Typography>
+                <Box position="relative" mb="72px">
+                  <TextField
+                  height="76px"
+                  placeholder='Search Exercise'
+                  value=""
+                  type="text"
+                  onChange={(e)=>{}}
+                  />
+
+                </Box>
+      </Stack>
+    </>
   )
 }
 
